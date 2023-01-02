ade2d2008215aa871102e345219a8eff6228cea3
unknown <s.liccardo022@gmail.com>
2023-01-02 16:54:07 +0100

    Aggiunte pagine di login e registrazione

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import addAvatar from '../img/addAvatar.png'
 
 function Login() {
   return (
-    <div>
-        <h1>Login</h1>
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <span className='Logo'>SimpleChat</span>
+            <span className='Title'>Accedi</span>
+            <form className='registerForm'>
+              <input type="email" placeholder='Email'></input>
+              <input type="password" placeholder='Password'></input>
+              <button>Accedi</button>
+              </form>
+              <p>Primo volta qui? Registrati</p>
+        </div>
     </div>
   )
 }
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import addAvatar from '../img/addAvatar.png'
 
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <span className='Logo'>SimpleChat</span>
+            <span className='Title'>Registrati</span>
+            <form className='registerForm'>
+              <input type="text" placeholder='Username'></input>
+              <input type="email" placeholder='Email'></input>
+              <input type="password" placeholder='Password'></input>
+              <input type="file" style={{display:"none"}} id="addAvatar"></input>
+              <label htmlFor='addAvatar' style={{cursor:"pointer"}}>
+              <img src={addAvatar}></img>
+              <span>Aggiungi un avatar</span>
+              </label>
+              <button>Registrati</button>
+              </form>
+              <p>Hai già un account? Login</p>
+        </div>
     </div>
   )
 }
