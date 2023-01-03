6084fe5f204317427da26aa0d25369f27bf8d67b
unknown <s.liccardo022@gmail.com>
2023-01-03 14:12:29 +0100

    Effettuate alcune modifiche allo stile

--- src/Elements/Chat.jsx
+++ src/Elements/Chat.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Cam from "../img/cam.png"
+import Add from "../img/add.png"
+import More from "../img/more.png"
+import Messages from './Messages'
+import Input from "./Input"
 
 function Chat() {
   return (
     <div className='chat'>
-      Chat
+      <div className="chatinfo">
+        <span>Jamal</span>
+        <div className="chaticons">
+          <img src={Cam} alt="cam"></img>
+          <img src={Add} alt="add"></img>
+          <img src={More} alt="more"></img>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
     </div>
   )
 }
--- src/Elements/Input.jsx
+++ src/Elements/Input.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function Input() {
   return (
-    <div>
-      
-    </div>
+    <div className='input'>Input</div>
   )
 }
 
