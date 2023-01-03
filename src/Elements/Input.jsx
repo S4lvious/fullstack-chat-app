1bfa8e93b2b073beb4c452f7b8277d1c3f5b0f2a
unknown <s.liccardo022@gmail.com>
2023-01-03 16:12:53 +0100

    Completato lo sviluppo dell'interfaccia

--- src/Elements/Input.jsx
+++ src/Elements/Input.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Img from '../img/img.png'
+import Attach from '../img/attach.png'
+
 
 function Input() {
   return (
-    <div className='input'>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Scrivi qualcosa...'></input>
+      <div className='send'>
+        <img src={Attach} alt=""></img>
+        <input type='file' style={{display:"none"}} id="file"></input>
+        <label htmlFor='file'>
+          <img src={Img} alt=''></img>
+        </label>
+        <button>Invia</button>
+      </div>
+    </div>
   )
 }
 
--- src/Elements/Message.jsx
+++ src/Elements/Message.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>
-      Message
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg" alt="" />
+        <span>Adesso</span>
+      </div>
+      <div className="messageContent">
+        <p>Ciao!</p>
+      <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg" alt="" />
+
+        
+      </div>
+
     </div>
   )
 }
