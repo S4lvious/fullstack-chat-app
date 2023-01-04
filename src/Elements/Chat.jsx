05a65115f156821f1af5724f244392849a17cb25
unknown <s.liccardo022@gmail.com>
2023-01-04 02:40:09 +0100

    Implmentate alcune query di join UID, e alcune modifiche di stile

--- src/Elements/Chat.jsx
+++ src/Elements/Chat.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import Cam from "../img/cam.png"
-import Add from "../img/add.png"
-import More from "../img/more.png"
-import Messages from './Messages'
-import Input from "./Input"
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
-function Chat() {
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
-    <div className='chat'>
-      <div className="chatinfo">
-        <span>Jamal</span>
-        <div className="chaticons">
-          <img src={Cam} alt="cam"></img>
-          <img src={Add} alt="add"></img>
-          <img src={More} alt="more"></img>
+    <div className="chat">
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
         </div>
       </div>
-      <Messages/>
+      <Messages />
       <Input/>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/Elements/Sidebar.jsx
+++ src/Elements/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Searchbar from './Searchbar'
+import Chats from './Chats'
+
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <Navbar/>
       <Searchbar/>
+      <Chats/>
     </div>
   )
 }
